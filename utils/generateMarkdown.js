6f0ed54eba29b8286363b744f725d066207059de
Sarah hagen <sarahhagen@sarahs-mbp.home>
2023-02-13 11:51:10 +0000

    add license types and read me

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,38 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const backticks = "```";
 
+  return `# ${data.title}
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  - [Description](#description)
+  - [License](#license)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  ## License
+  ${data.license}
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  To install dependancies please run the following command: 
+  ${backticks}
+  ${data.usage}
+  ${backticks}
+  ## Contributing
+  ${data.contributing}
+  ## Tests
+  To run tests please run the following command:
+  ${backticks}
+  ${data.tests}
+  ${backticks}
+  ## Questions
+  If you have any questions about the repo, openn an issue or contact me
+  directly at ${data.email}. You can find more of my work at ${data.github}.
 `;
 }
 
